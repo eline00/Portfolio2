ed13cd93efff1739ac8cc1989dca25d74029104c
gautte <s362066@oslomet.no>
2023-04-25 13:50:51 +0200

    Added a DRTP.py skeleton code inspired by the header.py file in the 2410 repo.

--- src/DRTP.py
+++ src/DRTP.py
@@ -1,32 +1,80 @@
+import sys
 import socket
+import time
+from struct import *
 
-class DRTP:
-    def __init__(self, local_port):
-        # Initialize the DRTP protocol with local port and other necessary variables
-        self.local_port = local_port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        
-        # Initialize other variables like sequence number, acknowledgment number, etc.
-        self.seq_num = 0
-        self.ack_num = 0
-        self.window_size = 64
-        # ...
+header_format = '!IIHH'
 
-    def connect(self, remote_address, remote_port):
-        # Perform three-way handshake to establish a reliable connection
-        # ...
+def create_packet(seq, ack, flags, win, data):
+    header = pack(header_format, seq, ack, flags, win)
+    packet = header + data
+    return packet
 
-    def send(self, data):
-        # Send data using DRTP protocol
-        # ...
 
-    def receive(self, size):
-        # Receive data using DRTP protocol
-        # ...
+def parse_header(header):
+    return unpack(header_format, header)
 
-    def close(self):
-        # Gracefully close the DRTP protocol and the connection
-        # ...
+
+def parse_flags(flags):
+    syn = flags & (1 << 3)
+    ack = flags & (1 << 2)
+    fin = flags & (1 << 1)
+    return syn, ack, fin
+
+
+def stop_and_wait_sender(file_data, sockfd, addr):
+    # Sender code for stop_and_wait
+    pass
+
+
+def GBN_sender(file_data, sockfd, addr):
+    # Sender code for GBN
+    pass
+
+
+def SR_sender(file_data, sockfd, addr):
+    # Sender code for SR
+    pass
+
+
+def stop_and_wait_receiver(sockfd, addr, output_file):
+    # Receiver code for stop_and_wait
+    pass
+
+
+def GBN_receiver(sockfd, addr, output_file):
+    # Receiver code for GBN
+    pass
+
+
+def SR_receiver(sockfd, addr, output_file):
+    # Receiver code for SR
+    pass
+
+
+def sender_main():
+    # Sender main function
+    pass
+
+
+def receiver_main():
+    # Receiver main function
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python DRTP.py sender|receiver <arguments>")
+        sys.exit(1)
+
+    mode = sys.argv[1]
+    if mode == "sender":
+        sender_main()
+    elif mode == "receiver":
+        receiver_main()
+    else:
+        print("Invalid mode. Choose 'sender' or 'receiver'.")
+        sys.exit(1)
 
     def settimeout(self, timeout):
         
